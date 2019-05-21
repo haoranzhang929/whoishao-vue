3cc5b4beb2649e2e61dd5736eb1156f26daab73c
Haoran Zhang <haoranzhang@icloud.com>
2019-05-21 01:08:26 +0100

    Change footer bg color
    Change footer bg color

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-footer app>
+  <v-footer color="transparent">
     <v-spacer></v-spacer>
     <div class="subheading font-weight-regular">&copy; {{ new Date().getFullYear() }} Haoran Zhang</div>
     <v-spacer></v-spacer>
