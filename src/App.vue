34e0066308f988505fbf16150b110bb9095da438
Haoran Zhang <haoranzhang@icloud.com>
2019-06-03 09:50:14 +0100

    Reduce fade out time
    Reduce fade out time

--- src/App.vue
+++ src/App.vue
@@ -40,7 +40,7 @@ html {
   transition: all 0.3s ease;
 }
 .slide-fade-leave-active {
-  transition: all 0.8s cubic-bezier(1, 0.5, 0.8, 1);
+  transition: all 0.3s cubic-bezier(1, 0.5, 0.8, 1);
 }
 .slide-fade-enter, .slide-fade-leave-to
 /* .slide-fade-leave-active below version 2.1.8 */ {
