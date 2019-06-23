eb47220f6d3b086efa25fe610ee485bead70e47d
Haoran Zhang <haoranzhang@icloud.com>
2019-06-23 20:03:33 +0100

    Add Projects page content
    Add Projects page content

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -1,11 +1,91 @@
 <template>
-  <v-container>
-    <v-layout row wrap></v-layout>
+  <v-container fill-height>
+    <v-layout row wrap justify-center>
+      <v-carousel height="550px">
+        <v-carousel-item v-for="(project,i) in projects" :key="i" :src="project.src">
+          <div class="project-card">
+            <div class="project-title mb-4">
+              <p class="headline project-name">{{project.name}}</p>
+            </div>
+            <div class="project-info">
+              <v-btn color="secondary" v-if="project.url" target="_blank" :href="project.url">URL</v-btn>
+              <v-btn color="secondary" v-if="project.code" target="_blank" :href="project.code">Code</v-btn>
+              <v-btn color="secondary" v-if="project.demo" target="_blank" :href="project.demo">Demo</v-btn>
+            </div>
+          </div>
+        </v-carousel-item>
+      </v-carousel>
+    </v-layout>
   </v-container>
 </template>
 
 <script>
 export default {
-  components: {}
+  data() {
+    return {
+      projects: [
+        {
+          name: "Image Finder",
+          src: "/Image-search.png",
+          demo: "http://hao-search.tk/",
+          code: "https://github.com/haoranzhang929/unsplash-image-finder"
+        },
+        {
+          name: "Audio Visual Tool",
+          src: "/audio-freq.png",
+          demo: "http://ec2-52-213-180-139.eu-west-1.compute.amazonaws.com/",
+          code:
+            "https://github.com/haoranzhang929/audio-frequency-visual-generators"
+        },
+        {
+          name: "ONE KICKS",
+          src: "/shoes.jpg",
+          url: "https://1-kicks.com/"
+        },
+        {
+          name: "Changing Places Ireland",
+          src: "/cp.png",
+          url: "https://changingplaces.ie/"
+        },
+        {
+          name: "Music Projects",
+          src: "/bc.jpg",
+          url: "https://ha0beatz.bandcamp.com/"
+        },
+        {
+          name: "Video Projects",
+          src: "/youtube.jpg",
+          url:
+            "https://www.youtube.com/channel/UCPIeJ9dP80qSbyOd2SGh4Jg/featured"
+        }
+      ]
+    };
+  }
 };
 </script>
+
+<style  >
+.v-responsive__content,
+.project-title,
+.project-info {
+  display: flex;
+  justify-content: center;
+}
+.project-card {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.project-title {
+  width: 300px;
+  height: 50px;
+  text-align: center;
+  background: rgba(201, 194, 186, 0.7);
+  color: #fff;
+  border-radius: 3px;
+}
+.project-name {
+  margin: 0;
+  align-self: center;
+}
+</style>
